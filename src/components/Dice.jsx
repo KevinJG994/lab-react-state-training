392b48ce376e43a89bc999644bf5aefbfbc2525d
KevinJG994 <KevinJG994@gmail.com>
2025-02-06 16:28:37 +0000

    done

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
+import diceEmpty from '../assets/images/dice-empty.png'
+import dice1 from '../assets/images/dice1.png'
+import dice2 from '../assets/images/dice2.png'
+import dice3 from '../assets/images/dice3.png'
+import dice4 from '../assets/images/dice4.png'
+import dice5 from '../assets/images/dice5.png'
+import dice6 from '../assets/images/dice6.png'
+
 
 function Dice() {
-    const [hiden, setHiden] = useState(false)
+    const [currentDice, setCurrentDice] = useState(diceEmpty)
+
+    const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
 
     const toggleImage = () => {
-        setHiden(true);
+        setCurrentDice(diceEmpty);
         setTimeout(() => {
-            <img src="./src/assets/images/dice6.png" alt="img-6"/>
-            setHiden(false);
-        }, 2000); 
+            const randomDice = diceImages[Math.floor(Math.random() * diceImages.length)];
+            setCurrentDice(randomDice);
+        }, 1000);
     };
 
     return (
         <div className="dice-container">
-            {hiden ? (
-                <img src="./src/assets/images/dice-empty.png" alt="img-vacia" onClick={toggleImage} />
-            )
-            
-            : (
-                <img src="./src/assets/images/dice3.png" alt="img-3" onClick={toggleImage} />
-            )}
+            <img src={currentDice} alt="dice" onClick={toggleImage} />
         </div>
     );
 }
